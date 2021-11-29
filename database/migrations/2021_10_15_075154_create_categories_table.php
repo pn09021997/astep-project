1b9900a41e4c3d5eac393ab823b567191d56beb5
Phuong Nguyen <pn0921997@gmail.com>
2021-11-29 21:15:11 +0700

    config migrate categories file

--- database/migrations/2021_10_15_075154_create_categories_table.php
+++ database/migrations/2021_10_15_075154_create_categories_table.php
@@ -17,6 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('image');
+            $table->string('description');
             $table->timestamps();
         });
     }
