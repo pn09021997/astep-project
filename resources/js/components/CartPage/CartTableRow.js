926f607d95301f0f75af1eeb0ebf29fb449802d0
quocviet0706 <nguyenquocviet0706@gmail.com>
2021-12-04 01:39:20 +0700

    update checkout cart

--- resources/js/components/CartPage/CartTableRow.js
+++ resources/js/components/CartPage/CartTableRow.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Button, Container } from "reactstrap";
+import { Modal, Form, Row, Col, FloatingLabel } from 'react-bootstrap';
 import axios from "axios";
 import Swal from "sweetalert2";
 
 export default function CartTableRow() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     // const history = useHistory();
 
     // const deleteCart = () => {
@@ -48,10 +53,10 @@ export default function CartTableRow() {
                             Soundless Speaker
                         </a>
                         <Button
-                        // onClick={deleteCart}
-                        className="btn-sm btn-block"
-                        color="danger"> Remove
-                    </Button>
+                            // onClick={deleteCart}
+                            className="btn-sm btn-block"
+                            color="danger"> Remove
+                        </Button>
                     </td>
                     <td className="quantity_cart">
                         <input type="number" value="1" className="text ng-pristine ng-untouched ng-valid" />
@@ -64,10 +69,35 @@ export default function CartTableRow() {
                 </tr>
                 <div className="Checlout_cart col text-center text-md-right">
                     <p class="h3 cart__subtotal"><span class="money">$398.00</span></p>
-                    <button type="submit" class="btn btn-theme gradient-theme btn-cart-checkout">Check Out</button>
+                    <button onClick={handleShow} type="submit" class="btn btn-theme gradient-theme btn-cart-checkout">Check Out</button>
                 </div>
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Thông tin địa chỉ</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Row>
+                            <Col md>
+                                <Form.Control type="email" placeholder="Address buy" />
+                            </Col>
+                            <Col md>
+                            <Form.Control type="email" placeholder="note product cart" />
+                            </Col>
+                        </Row>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary">Understood</Button>
+                    </Modal.Footer>
+                </Modal>
             </Container>
-
         </div>
     );
 }
