a7770dd93677a7170a98ffeb231f513a0efd0b10
Phuong Nguyen <pn0921997@gmail.com>
2021-12-11 07:12:37 +0700

    update branch reviews

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -18,173 +18,174 @@ class CommentController extends Controller
     // Id không phải là số nguyên
     // Không tìm thấy id
 
-    public function  WatchComment(Request $request){
+    public function  WatchComment(Request $request)
+    {
 
-        if (!$request->has('product_id')){
-            return  response()->json(['status'=>'Please add product id to find comment'],404);
+        if (!$request->has('product_id')) {
+            return  response()->json(['status' => 'Please add product id to find comment'], 404);
         }
         $product_id = $request->query('product_id');
-        if (!gettype($product_id)=='integer'){
-            return  response()->json(['status'=>"Product id not is number "]);
+        if (!gettype($product_id) == 'integer') {
+            return  response()->json(['status' => "Product id not is number "]);
         }
         try {
             $product = products::findOrFail($product_id);
-        }catch (\Exception $e){
-        return  response()->json(['status'=>'error not found product id ']);
+        } catch (\Exception $e) {
+            return  response()->json(['status' => 'error not found product id ']);
         }
 
-        $allComment = comment::where('product_id','=',$product->id)->get();
+        $allComment = comment::where('product_id', '=', $product->id)->get();
         // Khúc này  là check quyền của user
-        if(Auth::guard('api')->check()){
+        if (Auth::guard('api')->check()) {
             // If có đăng nhập thì khúc dưới chưa bao gồm trường hợp  admin delete comment
             // Check comment của đúng user đó hiển thị ra danh sách quyền trên comment đó .
 
-            foreach ($allComment as $comment){
-                if (Gate::allows('edit-comment',$comment)){
+            foreach ($allComment as $comment) {
+                if (Gate::allows('edit-comment', $comment)) {
                     $comment['quyen'] = 'edit,delete';
-                }
-                else{
+                } else {
                     $comment['quyen'] = "";
                 }
             }
             // Khúc này là quyền delete  của admin nha
-            if (Auth::user()->type == 1){
-                foreach ($allComment as $comment){
-                    if (Gate::allows('delete-comment',$comment)){
-                        if (empty($comment['quyen'])){
+            if (Auth::user()->type == 1) {
+                foreach ($allComment as $comment) {
+                    if (Gate::allows('delete-comment', $comment)) {
+                        if (empty($comment['quyen'])) {
                             $comment['quyen'] = 'delete';
                         }
                     }
                 }
             }
-
         }
         return $allComment;
-
     }
-    public  function WatchCommentNotAuth(Request  $request){
-        if (!$request->has('product_id')){
-            return  response()->json(['status'=>'Please add product id to find comment'],404);
+    public  function WatchCommentNotAuth(Request  $request)
+    {
+        if (!$request->has('product_id')) {
+            return  response()->json(['status' => 'Please add product id to find comment'], 404);
         }
         $product_id = $request->query('product_id');
-        if (!gettype($product_id)=='integer'){
-            return  response()->json(['status'=>"Product id not is number "]);
+        if (!gettype($product_id) == 'integer') {
+            return  response()->json(['status' => "Product id not is number "]);
         }
         try {
             $product = products::findOrFail($product_id);
-        }catch (\Exception $e){
-            return  response()->json(['status'=>'error not found product id ']);
+        } catch (\Exception $e) {
+            return  response()->json(['status' => 'error not found product id ']);
         }
-        $allComment = comment::where('product_id','=',$product->id)->get();
-        foreach ($allComment as $comment){
-           $comment['quyen'] = "";
+        $allComment = comment::where('product_id', '=', $product->id)->get();
+        foreach ($allComment as $comment) {
+            $comment['quyen'] = "";
         }
         return $allComment;
     }
 
 
-    public function postComment(Request $request,$product_id){
-        $product = products::where('id',$product_id)->first();
-        if($product){
-            $validator = Validator::make($request->all(),[
+    public function postComment(Request $request, $product_id)
+    {
+        $product = products::where('id', $product_id)->first();
+        if ($product) {
+            $validator = Validator::make($request->all(), [
                 'content' => 'required',
-                'rate'=> ''
+                'rate' => ''
             ]);
-            if($validator->fails()){
+            if ($validator->fails()) {
                 return response()->json([
                     'message' => 'Validation errors',
                     'status' => 500,
-                    'errors' => $validator->message()],422);
+                    'errors' => $validator->message()
+                ], 422);
             }
 
             $rate;
-            if($request->rate == null){
+            if ($request->rate == null) {
                 $rate = 1;
-            }else{
+            } else {
                 $rate = $request->rate;
             }
-                $comment = comment::create([
-                    'content' => $request->content,
-                    'product_id' => $product->id,
-                    'user_id' => $request->user()->id,
-                    'rate' => $rate
-                    ]);
-              
+            $comment = comment::create([
+                'content' => $request->content,
+                'product_id' => $product->id,
+                'user_id' => $request->user()->id,
+                'rate' => $rate
+            ]);
+
             $comment->load('user');
             return response()->json([
-                'message'=>'comment successfully',
+                'message' => 'comment successfully',
                 'comment' => $comment
-            ],200);
-        }else{
+            ], 200);
+        } else {
             return response()->json([
                 'message' => 'Product not found',
-            ],400);
+            ], 400);
         }
-}
+    }
 
-    public function editComment(Request $request,$id){
-        $comment = comment::with(['user'])->where('id',$id)->first();
-      
-        if($comment){
-            if($comment->user_id==$request->user()->id){
-                $validator = Validator::make($request->all(),[
+    public function editComment(Request $request, $id)
+    {
+        $comment = comment::with(['user'])->where('id', $id)->first();
+
+        if ($comment) {
+            if ($comment->user_id == $request->user()->id) {
+                $validator = Validator::make($request->all(), [
                     'content' => 'required',
-                    'rate'=> ''
+                    'rate' => ''
                 ]);
-                if($validator->fails()){
+                if ($validator->fails()) {
                     return response()->json([
                         'message' => 'Validation errors',
                         'status' => 500,
-                        'errors' => $validator->message()],422);
+                        'errors' => $validator->message()
+                    ], 422);
                 }
                 $comment->update([
                     'content' => $request->content,
-                    'rate'=> $request->rate
-                    
+                    'rate' => $request->rate
+
                 ]);
                 return response()->json([
                     'message' => "Comment successfully updated",
                     'comment' => $comment
-                ],200);
-            }else{
+                ], 200);
+            } else {
                 return response()->json([
                     'message' => "You can't edit this comment",
-                ],403);
+                ], 403);
             }
-        }else{
+        } else {
             return response()->json([
                 'message' => 'Comment not found',
-            ],400);
+            ], 400);
         }
     }
 
-    public function deleteComment(Request $request,$id){
-        $comment = comment::with(['user'])->where('id',$id)->first();
-        if($comment){
-            if (Auth::user()->type == 1){
+    public function deleteComment(Request $request, $id)
+    {
+        $comment = comment::with(['user'])->where('id', $id)->first();
+        if ($comment) {
+            if (Auth::user()->type == 1) {
                 $comment->delete();
                 return response()->json([
                     'message' => "Comment successfully deleted",
-                ],200);
-            }
-            else{
-                if($comment->user_id==$request->user()->id){
+                ], 200);
+            } else {
+                if ($comment->user_id == $request->user()->id) {
                     $comment->delete();
                     return response()->json([
                         'message' => "Comment successfully deleted",
-                    ],200);
-                }
-                else{
+                    ], 200);
+                } else {
                     return response()->json([
                         'message' => "You can't delete this comment",
-                    ],403);
+                    ], 403);
                 }
             }
-           
-        }else{
+        } else {
             return response()->json([
                 'message' => 'Comment not found',
-            ],400);
+            ], 400);
         }
     }
     /**
@@ -196,8 +197,8 @@ class CommentController extends Controller
 
     public function index()
     {
-                $comment = comment::all();
-                return response()->json($comment);
+        $comment = comment::all();
+        return response()->json($comment);
     }
 
     /**
@@ -218,7 +219,6 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
-            
     }
 
     /**
@@ -227,14 +227,14 @@ class CommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Request $request,$id)
+    public function show(Request $request, $id)
     {
-        $comment = comment::find($id);    
+        $comment = comment::find($id);
         if ($comment) {
-                return response()->json([
-                    'message' => 'comment found by id!',
-                    'comment' => $comment,
-                ]);
+            return response()->json([
+                'message' => 'comment found by id!',
+                'comment' => $comment,
+            ]);
         }
         return response()->json([
             'message' => 'comment not found!',
@@ -261,7 +261,6 @@ class CommentController extends Controller
      */
     public function update(Request $request, $id)
     {
-
     }
 
     /**
@@ -272,6 +271,5 @@ class CommentController extends Controller
      */
     public function destroy($id)
     {
-
     }
 }
